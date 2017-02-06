1b3134d08379d50c1fd3911e2f3c8476d2f39b33
Shariati, Amin <amin.shariati@sc.com>
2017-02-06 18:29:41 +0800

    Fixed ESLint issues

--- index.js
+++ index.js
@@ -3,83 +3,77 @@
 var chalk = require('chalk');
 
 module.exports = function (percentageValue, sizeInColumns, options) {
-    options = options || {};
+  options = options || {};
+  var MAXIMUM = process.stdout.columns;
+  var PROGRESS_BAR_COLOR = 'green';
+  var counter = 0;
 
-    var MINIMUM = 0;
-    var MAXIMUM = process.stdout.columns;
-    var PROGRESS_BAR_COLOR = 'green';
+  // NOTE: A complete list can be found at https://en.wikipedia.org/wiki/Block_Elements
+  var PROGRESS_BAR_CHARACTER_DESIGN = '▄';
 
-    // NOTE: A complete list can be found at https://en.wikipedia.org/wiki/Block_Elements
-    var PROGRESS_BAR_CHARACTER_DESIGN = '▄';
+  if (options.color) {
+    PROGRESS_BAR_COLOR = options.color;
+  }
 
-
-    if (options.color) {
-        PROGRESS_BAR_COLOR = options.color;
-    }
-
-    if (options.design) {
-        if(options.design.length > 1) {
-            PROGRESS_BAR_CHARACTER_DESIGN = options.design.charAt(0);
-        } else {
-            PROGRESS_BAR_CHARACTER_DESIGN = options.design;
-        }
-        
-    }
-
-    var progressCharacter = PROGRESS_BAR_CHARACTER_DESIGN;
-    var progressBar = '';
-    if (sizeInColumns > process.stdout.columns || sizeInColumns < 1) {
-        sizeInColumns = MAXIMUM;
-    }
-    if (percentageValue > 100) {
-        percentageValue = 100;
-    }
-    if (percentageValue < 0) {
-        percentageValue = 0;
-    }
-    for (var counter = 0; counter < sizeInColumns; counter++) {
-        progressBar = chalk.gray(progressCharacter);
-        process.stdout.write(`${progressBar}`);
-    }
-    var value = Math.floor((percentageValue * sizeInColumns) / 100);
-    for (var counter = 0; counter < value; counter++) {
-        progressBar = chalk.blue(progressCharacter);
-        switch (PROGRESS_BAR_COLOR) {
-            case 'red':
-                progressBar = chalk.red(progressCharacter);
-                break;
-            case 'green':
-                progressBar = chalk.green(progressCharacter);
-                break;
-            case 'yellow':
-                progressBar = chalk.yellow(progressCharacter);
-                break;
-            case 'magenta':
-                progressBar = chalk.magenta(progressCharacter);
-                break;
-            case 'cyan':
-                progressBar = chalk.cyan(progressCharacter);
-                break;
-            case 'white':
-                progressBar = chalk.white(progressCharacter);
-                break;
-            case 'gray':
-                progressBar = chalk.gray(progressCharacter);
-                break;
-            case 'blue':
-            default:
-                progressBar = chalk.blue(progressCharacter);
-                break;
-        }
-        process.stdout.cursorTo(counter)
-        process.stdout.write(`${progressBar}`);
-    }
-
-    if (options.hasNewLine) {
-        process.stdout.write(`\n`);
+  if (options.design) {
+    if (options.design.length > 1) {
+      PROGRESS_BAR_CHARACTER_DESIGN = options.design.charAt(0);
     } else {
-        process.stdout.cursorTo(sizeInColumns + 1);
-        process.stdout.write(` `);
+      PROGRESS_BAR_CHARACTER_DESIGN = options.design;
     }
-
+  }
+  var progressCharacter = PROGRESS_BAR_CHARACTER_DESIGN;
+  var progressBar = '';
+  if (sizeInColumns > process.stdout.columns || sizeInColumns < 1) {
+    sizeInColumns = MAXIMUM;
+  }
+  if (percentageValue > 100) {
+    percentageValue = 100;
+  }
+  if (percentageValue < 0) {
+    percentageValue = 0;
+  }
+  for (counter = 0; counter < sizeInColumns; counter++) {
+    progressBar = chalk.gray(progressCharacter);
+    process.stdout.write(`${progressBar}`);
+  }
+  var value = Math.floor((percentageValue * sizeInColumns) / 100);
+  for (counter = 0; counter < value; counter++) {
+    progressBar = chalk.blue(progressCharacter);
+    switch (PROGRESS_BAR_COLOR) {
+      case 'red':
+        progressBar = chalk.red(progressCharacter);
+        break;
+      case 'green':
+        progressBar = chalk.green(progressCharacter);
+        break;
+      case 'yellow':
+        progressBar = chalk.yellow(progressCharacter);
+        break;
+      case 'magenta':
+        progressBar = chalk.magenta(progressCharacter);
+        break;
+      case 'cyan':
+        progressBar = chalk.cyan(progressCharacter);
+        break;
+      case 'white':
+        progressBar = chalk.white(progressCharacter);
+        break;
+      case 'gray':
+        progressBar = chalk.gray(progressCharacter);
+        break;
+      case 'blue':
+      default:
+        progressBar = chalk.blue(progressCharacter);
+        break;
+    }
+    process.stdout.cursorTo(counter);
+    process.stdout.write(`${progressBar}`);
+  }
+  if (options.hasNewLine) {
+    process.stdout.write(`\n`);
+  } else {
+    process.stdout.cursorTo(sizeInColumns + 1);
+    process.stdout.write(` `);
+  }
 };
